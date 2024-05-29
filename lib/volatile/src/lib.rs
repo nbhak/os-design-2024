bd41148a39d1d14c87d669aae3bc1337910786cd
nbhak <nbhak@stanford.edu>
2024-05-28 23:17:13 -0700

    Updated volatile to compile properly

--- lib/volatile/src/lib.rs
+++ lib/volatile/src/lib.rs
@@ -1,13 +1,12 @@
 #![feature(decl_macro)]
-#![feature(optin_builtin_traits)]
-
+#![feature(negative_impls)] // Enable negative impls
 #![no_std]
 
-mod traits;
 mod macros;
+mod traits;
 
-pub use traits::*;
 use macros::*;
+pub use traits::*;
 
 /// Reexports all of the traits in this crate.
 ///
@@ -18,8 +17,8 @@ use macros::*;
 /// use volatile::prelude::*;
 /// ```
 pub mod prelude {
-	#[doc(no_inline)]
-    pub use super::{Readable, Writeable, ReadableWriteable, Wrapper};
+    #[doc(no_inline)]
+    pub use super::{Readable, ReadableWriteable, Wrapper, Writeable};
 }
 
 /// A wrapper type that enforces **read-only** _volatile_ accesses to a raw
@@ -55,29 +54,29 @@ pub struct Unique<T>(T);
 // Implementations for `ReadVolatile`.
 ptr!(ReadVolatile, |self| &self.0);
 readable!(ReadVolatile, |self| &self.0);
-unsafe impl<T: Send> Send for ReadVolatile<T> {  }
-impl<T> !Sync for ReadVolatile<T> {  }
+unsafe impl<T: Send> Send for ReadVolatile<T> {}
+impl<T> !Sync for ReadVolatile<T> {}
 
 // Implementations for `Volatile`.
 ptr!(Volatile, |self| &self.0);
 readable!(Volatile, |self| &self.0);
 writeable!(Volatile, |self| &mut self.0);
 readable_writeable!(Volatile);
-unsafe impl<T: Send> Send for Volatile<T> {  }
-impl<T> !Sync for Volatile<T> {  }
+unsafe impl<T: Send> Send for Volatile<T> {}
+impl<T> !Sync for Volatile<T> {}
 
 // Implementations for `WriteVolatile`.
 writeable!(WriteVolatile, |self| &mut self.0);
 ptr!(WriteVolatile, |self| &self.0);
-unsafe impl<T: Send> Send for WriteVolatile<T> {  }
-impl<T> !Sync for WriteVolatile<T> {  }
+unsafe impl<T: Send> Send for WriteVolatile<T> {}
+impl<T> !Sync for WriteVolatile<T> {}
 
 // Implementations for `Reserved`.
 ptr!(Reserved, |self| &self.0);
 
 // Implementations for `Unique`.
-unsafe impl<R: Wrapper> Send for Unique<R> where <R as Wrapper>::Inner: Send {  }
-unsafe impl<R: Wrapper> Sync for Unique<R> where <R as Wrapper>::Inner: Sync {  }
+unsafe impl<R: Wrapper> Send for Unique<R> where <R as Wrapper>::Inner: Send {}
+unsafe impl<R: Wrapper> Sync for Unique<R> where <R as Wrapper>::Inner: Sync {}
 
 impl<T, R: Readable<T>> Readable<T> for Unique<R> {
     #[inline(always)]
@@ -94,4 +93,8 @@ impl<T, R: Writeable<T>> Writeable<T> for Unique<R> {
 }
 
 impl<T, R: ReadableWriteable<T>> ReadableWriteable<T> for Unique<R>
-    where T: ::core::ops::BitAnd<Output = T>, T: ::core::ops::BitOr<Output = T> { }
+where
+    T: ::core::ops::BitAnd<Output = T>,
+    T: ::core::ops::BitOr<Output = T>,
+{
+}
